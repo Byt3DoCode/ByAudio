b12982f3bcdbc23f71d066fd2448eb1151a1eff2
Byt3 <shaow9csgo@gmail.com>
2023-10-20 04:03:01 +0700

    folder frame

--- app/src/main/java/com/byt3/byaudio/view/MainActivity.java
+++ app/src/main/java/com/byt3/byaudio/view/MainActivity.java
@@ -1,8 +1,10 @@
-package com.byt3.byaudio;
+package com.byt3.byaudio.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.byt3.byaudio.R;
 
 public class MainActivity extends AppCompatActivity {
 
