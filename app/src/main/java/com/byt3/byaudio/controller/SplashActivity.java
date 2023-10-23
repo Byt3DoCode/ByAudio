ece819111609777e164283e01e5777411e2984b8
Byt3 <shaow9csgo@gmail.com>
2023-10-23 15:24:20 +0700

    week4-day2

---
+++ app/src/main/java/com/byt3/byaudio/controller/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.byt3.byaudio.controller;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import androidx.appcompat.app.AppCompatActivity;
+import com.byt3.byaudio.R;
+
+import java.util.Objects;
+
+@SuppressLint("CustomSplashScreen")
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        Objects.requireNonNull(getSupportActionBar()).hide();
+        Handler handler = new Handler();
+        handler.postDelayed(this::nextActivity, 2000);
+    }
+
+    private void nextActivity() {
+        Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+        finishAffinity();
+    }
+}
