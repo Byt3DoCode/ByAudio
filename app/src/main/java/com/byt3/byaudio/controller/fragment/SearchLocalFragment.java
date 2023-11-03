4528afafaa2b71eee4da3d4694ad5dbb06b1a91f
Byt3 <shaow9csgo@gmail.com>
2023-11-03 13:29:12 +0700

    tuan5-thu6

--- app/src/main/java/com/byt3/byaudio/controller/fragment/SearchLocalFragment.java
+++ app/src/main/java/com/byt3/byaudio/controller/fragment/SearchLocalFragment.java
@@ -16,6 +16,11 @@ public class SearchLocalFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_search_local, container, false);
+        initView(view);
         return view;
+    }
+
+    private void initView(View view) {
+
     }
 }
